cc5043fe319fc444f91b209bcdd27a037b77f1b5
Omar Alvarez <omajul2@hotmail.com>
2016-05-10 17:38:03 +0200

    Add feature for minimum temperature equals to 10 degrees

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -10,4 +10,20 @@ describe('Thermostat', function(){
 	it('starts at 20 degrees', function(){
 		expect(thermostat.getTemperature()).toEqual(20);
 	});
+
+	it('increases the temperature with the up button', function(){
+		thermostat.up();
+		expect(thermostat.getTemperature()).toEqual(21);
+	});
+
+	it('descreases the temperature with the down button', function(){
+		thermostat.down();
+		expect(thermostat.getTemperature()).toEqual(19);
+	});
+
+	it('limits the minimum temperature to 10 degrees', function(){
+		thermostat._temperature = 10;
+		thermostat.down();
+		expect(thermostat.getTemperature()).toEqual(10);
+	});
 });
--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -2,8 +2,19 @@
 
 function Thermostat(){
 	this._temperature = 20;
+	this._MINIMUM_TEMPERATURE = 10;
 }
 
 Thermostat.prototype.getTemperature = function() {
 	return this._temperature;
+};
+
+Thermostat.prototype.up = function() {
+	this._temperature += 1;
+};
+
+Thermostat.prototype.down = function() {
+	if(this.getTemperature() > this._MINIMUM_TEMPERATURE){
+		this._temperature -= 1;
+	}
 };
