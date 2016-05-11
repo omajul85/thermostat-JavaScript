e3f0676c91d77d101ce021fa094c600a5ac1608b
Omar Alvarez <omajul2@hotmail.com>
2016-05-11 09:59:14 +0200

    Toggle PSM images on click

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    $("h1").click(function(){
-        $(this).hide();
-    });
+	$("h1").click(function(){
+		$(this).hide();
+	});
+
+	$("#PSM").click(function(){
+		if($(this).attr("src") == "public/images/psm_off.png"){
+			$(this).attr("src", "public/images/psm_on.png");
+		} else {
+			$(this).attr("src", "public/images/psm_off.png");
+		}
+	});
 });
