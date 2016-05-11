a4098c4fc6d40dd5be164b64486911fb65bce345
Omar Alvarez <omajul2@hotmail.com>
2016-05-11 15:59:34 +0200

    Wunderground API integration

---
+++ public/javascripts/wunderground.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+	function displayWeather(country, city){
+		$.ajax({
+			url : "http://api.wunderground.com/api/403cd5091c5612ec/conditions/q/" + country + "/" + city + ".json",
+			dataType : "jsonp",
+			success : function(parsed_json) {
+				var location = parsed_json['current_observation']['display_location']['city'];
+				for(item in location){
+					console.log(item);
+				}
+				var temp_c = parsed_json['current_observation']['temp_c'];
+				$('#current-temperature').html("<h1>Current temperature in " + location + " is " + temp_c + " &#8451;</h1>");
+			}
+		});
+	}
+
+	// By default
+	displayWeather('France', 'Paris');
+
+	$("#select-location").submit(function(event){
+		event.preventDefault();
+		var country = $("#current-country").val();
+		var city = $("#current-city").val();
+		displayWeather(country, city);
+	});
+});
